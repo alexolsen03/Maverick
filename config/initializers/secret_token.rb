128d222ebe4e2f78960ebd99722e701d41defd02
alex <alexolsen03@gmail.com>
2014-02-17 19:36:29 -0600

    adding existing files to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocktemplate::Application.config.secret_key_base = '1a67e53a7d2217e5180c518fd48fe7b8ac710a7542948cad48174bc067164db262dd9da1e026fac49c4d2d031b2518b3c0129321f9a55936b7d55fbb52e1e861'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blocktemplate::Application.config.session_store :cookie_store, key: '_blocktemplate_session'
